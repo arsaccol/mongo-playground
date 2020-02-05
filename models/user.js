b9f69ce15e721eae2e063b6e02238076af687c9f
Alexandre R. Saccol <arsaccol@gmail.com>
2020-02-05 16:20:30 -0300

    Implement rudimentary user signup I guess

--- models/user.js
+++ models/user.js
@@ -1,11 +1,20 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.model('User', {
-    _id: mongoose.Schema.Types.ObjectId,
-    email: { type: mongoose.Schema.Types.email },
-    username: { type: String },
-    name: { type: mongoose.Schema.Types.name },
-    password: { type: mongoose.Schema.Types.password }
+//const userModel = mongoose.model('User', {
+//    _id: mongoose.Schema.Types.ObjectId,
+//    email: { type: mongoose.Schema.Types.email },
+//    username: { type: String },
+//    name: { type: mongoose.Schema.Types.name },
+//    password: { type: mongoose.Schema.Types.password }
+//});
+
+const user_model = mongoose.model('User', {
+    _id: mongoose.Types.ObjectId,
+    email: String,
+    username: String,
+    name: String,
+    password: String,
+    registered_date: Date
 });
 
-module.exports = userSchema;
+module.exports = user_model;
--- routes/signup.js
+++ routes/signup.js
@@ -4,27 +4,73 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
 
+const User = require('../models/user');
+
+const url = require('url');
+const db_url = url.resolve(process.env.MONGODB_PATH, process.env.DB_NAME);
 
 router.use(express.json());
 
 
 router.get('/', (req, res) => {
-    mongoose.connect(process.env.MONGODB_PATH + process.env.DB_NAME, {useNewUrlParser: true});
+    //mongoose.connect(url.resolve(process.env.MONGODB_PATH, process.env.DB_NAME), {useNewUrlParser: true});
 
 
 
-    res.send('signup GETTO!');
+    res.send(
+        url.resolve(db_url, 'bugabuga')
+    );
 });
 
 // Expected route: /api/signup
 router.post('/', (req, res) => {
     const { email, username, name, password } = req.body;
     
-    const hashed_pwd = bcrypt.hashSync(password, 10);
+    mongoose.connect(db_url, {useNewUrlParser: true, useUnifiedTopology: true});
+    let db = mongoose.connection;
+
+    db.on('error', console.error.bind(console, 'connection error:'));
     
+    db.once('open', () => {
+        const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: email,
+            username: username,
+            name: name,
+            password: bcrypt.hashSync(password, 10),
+            registered_date: Date.now()
+        });
+
+
+        // Check whether user with this email exists
+        User.find({email: email}, function(err, users) {
+            if(err)
+                console.log(error);
+            // Save if it doesn't
+            else if(users.length === 0) {
+                user.save( function(err, user) {
+                    console.log(JSON.stringify(user, null, 2));
+                    res.json({
+                        status: 200,
+                        message: 'User successfully created'
+                    })
+                });
+            }
+            else {
+                res.json({
+                    status: 409,
+                    message: 'The user you are trying to create already exists'
+                });
+            }
+        });
+
+
+        
+
+        //console.log(user);
+        //res.send(JSON.stringify(user, null, 2));
+    });
     
-    //console.log(new_user_data);
-    res.send(JSON.stringify({ email, username, name,  hashed_pwd }, null, 2));
 });
 
 
