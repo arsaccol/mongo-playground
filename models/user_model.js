12407355d7a712963b951cc658bc49193487f45f
Alexandre R. Saccol <arsaccol@gmail.com>
2020-02-12 14:50:11 -0300

    Improvements in login system; now returning database _id as id in get request and login

--- models/user_model.js
+++ models/user_model.js
@@ -6,21 +6,33 @@ const jwt = require('jsonwebtoken');
 const user_schema = new mongoose.Schema({
     email: { 
         type: String,
-        //required: true,
+        required: true,
         //index: { unique: true }
     },
     username: {
         type: String,
-        //required: true
+        required: true
     },
     name: {
         type: String,
-        //required: true
+        required: true
     },
     password: {
         type: String,
-        //required: true
+        required: true
     },
+    
+    //customer: {
+    //    type: {
+
+    //        
+
+    //    }
+
+    //},
+    
+
+    
 });
 
 user_schema.statics.findByCredentials = async (email, password) => {
@@ -37,13 +49,28 @@ user_schema.statics.findByCredentials = async (email, password) => {
         return user;
 };
 
-user_schema.statics.findByUsername = async(username) => {
-    const user = await user_model.findOne({username: username});
+user_schema.statics.findByIdWithoutPassword = async(userid) => {
+    //const user = await user_model.findOne({username: username});
+    const db_user = await user_model.findById(userid);
     
-    if(!user)
-        throw new Error({error: `User with username ${username} not found`});
-    else
-        return user;
+    
+    
+    if(!db_user)
+        throw new Error({error: `User with id ${userid} not found`});
+    else {
+        const no_pass_user = {
+            id: db_user._id,
+            username: db_user.username,
+            email: db_user.email,
+            name: db_user.name
+        }
+        
+
+        console.log(JSON.stringify(no_pass_user, null, 2));
+
+
+        return no_pass_user;
+    }
 };
 
 user_schema.pre('save', async function (next) {
@@ -55,11 +82,19 @@ user_schema.pre('save', async function (next) {
 });
 
 
+
+user_schema.methods.update = async function() {
+
+
+
+}
+
+
 user_schema.methods.generateAuthToken = async function() { 
     const user = this;
     //const token = jwt.sign({email: user.email, username: user.username, login_date: Date.now()}, process.env.JWT_KEY, {expiresIn: "1h"});
     // Disable token expiry for testing
-    const token = jwt.sign({email: user.email, username: user.username, login_date: Date.now()}, process.env.JWT_KEY);
+    const token = jwt.sign({id: user._id, email: user.email, username: user.username, login_date: Date.now()}, process.env.JWT_KEY);
     return token;
 };
 
--- routes/users_router.js
+++ routes/users_router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const users_router = express.Router();
 const jwt = require('jsonwebtoken');
 const auth = require('../middleware/authorization');
+const url = require('url');
 
 const User = require('../models/user_model');
 
@@ -21,6 +22,7 @@ users_router.post('/login', async (req, res) => {
         //const token = jwt.sign({username: authenticated_user.username, signin_time: Date.now()}, process.env.JWT_KEY, {expiresIn: "1 hour"});
         const token = await authenticated_user.generateAuthToken();
         res.status(200).json({ token: token, user: { 
+            id: authenticated_user.id,
             email: authenticated_user.email,
             username: authenticated_user.email
         }});
@@ -34,47 +36,50 @@ users_router.post('/login', async (req, res) => {
     }
 });
 
-users_router.get('/:username', auth, (req, res) => {
-    const payload = req.token_payload;
-    const username = req.params.username;
-    
-    //res.status(200).json(
-    //    payload
-    //);
-    
-    res.status(200).json({
-        username: username
-    })
 
-   
-    //if(username === payload.username) {
-    //    try {
-    //        const user_data = User.findByUsername(username);
-    //        res.status(200).json(user_data);
-    //    }
-    //    catch(err) {
-    //        console.log(err)
-    //        res.status(401).json(err);
-    //    }
-    //}
-    //else {
-    //    res.status(401).json({
-    //        status: 'Unauthorized',
-    //        message: `${payload.username} is not the same as ${username}`
-    //    })
-    //}
+users_router.put('/:username', auth, (req, res) => {
+
+
+
+});
+
+
+users_router.get('/id/:userid', auth, async (req, res) => {
+    const payload = req.token_payload;
+    const userid = req.params.userid;
+    const uri = req.originalUrl;
+    
+    try {
+        const user_safe = await User.findByIdWithoutPassword(userid);
+        user_safe.uri = uri
+        res.status(200).json(user_safe);
+    }
+    catch(err) {
+        res.status(404).json({
+            status: 'Not found',
+            uri: uri,
+            message: 'Could not find user'
+        })
+
+    }
 });
 
 users_router.post('/signup', async (req, res) => {
     console.log('buga');
     
-    const {email, password} = req.body;
     
     try {
         const user = new User(req.body);
+        
+        const public_user = {_id, email, username, name} = user;
+        
+
         await user.save();
 
-        res.status(200).json(user);
+        res.status(201).json({
+            status: 'Created',
+            user: public_user
+        });
     }
     catch(err) {
         console.log(err);
